961722e63af4716a81edec78f6f9701e482e85b2
Jorge Parreira <44405682+JorgeParreira@users.noreply.github.com>
2024-04-16 18:33:21 +0100

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
         System.out.println(department);
+
+        Seller seller = new Seller(1, "Jorge Parreira", "jorge@mail.com", new Date(), 2000.0, department);
+        System.out.println(seller);
     }
 }
