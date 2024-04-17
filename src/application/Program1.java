b0ebff10b6e82c900f3f84a022ea86cd4a35a9d4
Jorge Parreira <44405682+JorgeParreira@users.noreply.github.com>
2024-04-17 17:33:31 +0100

    DepartmentDaoJDBC implementation

--- src/application/Program1.java
+++ src/application/Program1.java
@@ -10,7 +10,7 @@ import java.util.Date;
 import java.util.List;
 
 
-public class Main {
+public class Program1 {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
